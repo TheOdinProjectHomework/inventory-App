7b3bc9b6efe921d86816188e94e95965a0bfd34e
Cristian Contreras <contreras.cristian994@gmail.com>
2026-02-19 11:07:46 -0500

    routes added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
+import categoryRouter from "./routes/categoryRouter.js"
+import itemRouter from "./routes/itemRouter.js"
 
 dotenv.config();
 const port = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
+app.use("/api/category", categoryRouter);
+app.use("/api/items", itemRouter);
 
 app.get("/", (req, res) => {
     res.json("Hello World!");
