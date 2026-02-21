73c01d1d4b1f204dd7d90314d55f7378a805c12b
Cristian Contreras <contreras.cristian994@gmail.com>
2026-02-21 11:33:04 -0500

    frontend initialized

--- backend/routes/itemRouter.js
+++ backend/routes/itemRouter.js
@@ -1,10 +1,9 @@
 import express from "express"
+import { getAllItems } from "../controllers/item.controller.js";
 
 const itemRouter = express.Router();
 itemRouter.use(express.json());
 
-itemRouter.get("/", (req, res) => {
-    res.json("Items route");
-})
+itemRouter.get("/", getAllItems);
 
 export default itemRouter;
