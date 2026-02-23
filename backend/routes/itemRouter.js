d8450a245c70eff6f87ae03a08e8c221b8c5894b
Cristian Contreras <contreras.cristian994@gmail.com>
2026-02-22 23:39:24 -0500

    createItem added db

--- backend/routes/itemRouter.js
+++ backend/routes/itemRouter.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getAllItems } from "../controllers/item.controller.js";
+import { createItem, getAllItems } from "../controllers/item.controller.js";
 
 const itemRouter = express.Router();
 itemRouter.use(express.json());
 
 itemRouter.get("/", getAllItems);
+itemRouter.post("/", createItem);
 
 export default itemRouter;
